491a35edf65f7a7dfb763db11d80d5122c4274bb
Matthew Ritter <matthew.ritter@formidable.com>
2023-06-21 14:44:12 -0400

    Extra

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Matthew Ritter",
-  description: "My Portfolio and a little bit about me.",
+  description: "My Portfolio and a little bit about me and what I do.",
 };
 
 export default function RootLayout({
