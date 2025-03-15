b340002f4285bf453a5ec34a4a16aa2725bc86b8
Matthew Ritter <matthewwilliamritter@gmail.com>
2025-03-14 21:41:32 -0400

    Fix animation

--- src/components/RiveComponent.tsx
+++ src/components/RiveComponent.tsx
@@ -6,7 +6,7 @@ import React from "react";
 
 export const RiveComponent = ({ src }: { src: string }) => {
   return (
-    <div className="animate-appear fixed flex flex-col top-0 right-0 sm:h-screen w-[500px] blur-xl sm:blur-xs z-0">
+    <div className="animate-appear fixed flex flex-col top-0 right-0 h-screen w-[500px] blur-md sm:blur-xs z-0">
       <Rive src={src} />
       <Rive src={src} />
     </div>
